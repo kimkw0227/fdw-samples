dc09c4b468fcb44d92d79d69f0689624c8ebcbcb
KingKwan <kmkim@bitnine.net>
2019-06-25 15:20:41 +0900

    execution error fixed#7

--- ctifdw/__init__.py
+++ ctifdw/__init__.py
@@ -408,7 +408,7 @@ class ThreatMinerIpExtraForeignDataWrapper(ForeignDataWrapper):
     def execute(self, quals, columns):
         intrusion_set_list = []
         conn_string = _conn_string
-        query = "MATCH (a:ioc) WHERE a.type='ip' AND a.value <> '-' RETURN DISTINCT a.value AS ip_value"
+        query = "MATCH (a:ioc) WHERE a.type='ip' AND a.value <> '-' AND a.value <> '' RETURN DISTINCT a.value AS ip_value"
         report_api = "https://api.threatminer.org/v2/host.php"
         try:
             conn = ag.connect(conn_string)
@@ -452,7 +452,7 @@ class VirusTotalForeignDataWrapper(ForeignDataWrapper):
     def execute(self, quals, columns):
         intrusion_set_list = []
         conn_string = _conn_string
-        query = "MATCH (a:ioc) WHERE a.type='md5_hash' RETURN DISTINCT a.value AS hash_value"
+        query = "MATCH (a:ioc) WHERE a.type='md5_hash' AND a.value <> '' RETURN DISTINCT a.value AS hash_value"
         report_api = "https://www.virustotal.com/ui/files/"
         try:
             conn = ag.connect(conn_string)
