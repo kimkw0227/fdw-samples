3e648ec402f93a2f9307c5391d86c6d6d57374c6
KingKwan <kmkim@bitnine.net>
2019-06-26 09:45:24 +0900

    new wrapper(IPWhoisForeignDataWrapper) added(fix#2)

--- ctifdw/__init__.py
+++ ctifdw/__init__.py
@@ -409,7 +409,7 @@ class IPWhoisForeignDataWrapper(ForeignDataWrapper):
     def execute(self, quals, columns):
         intrusion_set_list = []
         conn_string = _conn_string
-        query = "MATCH (a:ioc) WHERE a.type=\'ip\' AND a.value <> \'-\' AND a.value <> \'\' RETURN DISTINCT a.value AS ip_value"
+        query = "MATCH (a:ioc) WHERE a.type=\'ip\' AND a.value <> \'-\' AND a.value <> \'\' AND a.value <> \'0.0.0.0\' RETURN DISTINCT a.value AS ip_value"
 
         try:
             conn = ag.connect(conn_string)
